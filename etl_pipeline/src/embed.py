6603766a9a1c389a284b3ee14b316ec6ec77b323
Kennethbaole <kennethble@gmail.com>
2025-12-07 01:40:19 -0500

    Voodoo Code

--- etl_pipeline/src/embed.py
+++ etl_pipeline/src/embed.py
@@ -7,8 +7,9 @@ import openai
 import json
 import pathlib
 import os
-import numpy as np
-import faiss
+import numpy as np # Matrix math library 
+import faiss    # Vector database library 
+from tqdm import tqdm # Progress bar 
 
 # paths
 current_dir = pathlib.Path(__file__).parent
@@ -19,24 +20,38 @@ api_key = os.getenv("OPENAI_API_KEY")
 client = openai.OpenAI(api_key=api_key)
 
 def get_embedding(text):
-    text = text.replace("\n", " ") # replacing new lines with space 
+    text = text.replace("\n", " ") # replacing new lines with space  
     response = client.embeddings.create(input=text, model="text-embedding-3-small")
     return response.data[0].embedding
 
 if __name__ == "__main__":
     input_file = DATA_DIR / "processed" / "all_master_chunks.json"
-    # loading chunks 
+    
+    output_dir = DATA_DIR / "vector_db" # checks if the vector database exist, if not, create it 
+    if not output_dir.exists(): 
+        output_dir.mkdir(parents=True)
+
     with open(input_file, 'r') as f:
         chunks = json.load(f)
 
-    # two buckets:
-    vector_list = [] # holds vectors (list of numbers)
-    metadata_list = [] # holds text/page info 
+    vector_list = []
+    metadata_list = []
 
-    for i, item in enumerate(chunks[:5]):
-        vector = get_embedding(item['text']) # item is a dictionary, only extract 'text'
-        vector_list.append(vector) # appending vector 
-        metadata_list.append(item) # appending the dictionary 
-    
-    if len(vector_list) > 0:
-        print(f"Vector Dimension: {len(vector_list[0])}") # should be 1536
+    for item in tqdm(chunks): # tqdm wraps the list to show a progress bar in terminal 
+        try:
+            vector = get_embedding(item['text'])
+            vector_list.append(vector)
+            metadata_list.append(item)
+        except Exception as e:
+            print(f"Skipping chunk due to error: {e}")
+
+    vectors = np.array(vector_list).astype('float32') # np.array converts list of lists into a matrix 
+
+    # building the database
+    dimension = 1536
+    # IndexFlatL2 -> when search, calculate distance between vectors to find closest match 
+    index = faiss.IndexFlatL2(dimension) # creating an empty database expecting 1536 - dim vectors
+    index.add(vectors) # Add matrix to database 
+    faiss.write_index(index, str(output_dir / "eliza.index"))
+    with open(output_dir / "metadata.json", "w") as f:
+        json.dump(metadata_list, f)
