5f808e07753fbf9073e29a1d0012f49028cac3d7
Kennethbaole <kennethble@gmail.com>
2025-12-07 15:54:14 -0500

    loading index

--- backend/api/logic.py
+++ backend/api/logic.py
@@ -3,14 +3,27 @@ import pathlib
 import os
 import numpy as np # Matrix math library 
 import faiss    # Vector database library 
-from tqdm import tqdm # Progress bar 
+
 
 current_dir = pathlib.Path(__file__).parent
 DATA_DIR = current_dir.parent.parent / "data"
+vector_db_path = DATA_DIR / "vector_db"
 
 def load_index():
+    global INDEX, METADATA
+
+    input_elizaIndex = vector_db_path / "eliza.index"
+    input_metadata = vector_db_path / "metadata.json"
+
+    INDEX = faiss.read_index(str(input_elizaIndex)) # load the index 
+    with open(input_metadata, 'r') as f: # load metadata 
+        METADATA = json.load(f)
+
+def search(query):
     pass
 
-def search():
+def generate_answer(query, context):
     pass
 
+if __name__ == "__main__":
+    pass
