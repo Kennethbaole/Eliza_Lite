5380ecf45c8eea0a5c504fbdd3e60c1a0554f188
Kennethbaole <kennethble@gmail.com>
2025-12-12 15:04:55 -0500

    jk

--- backend/api/logic.py
+++ backend/api/logic.py
@@ -55,7 +55,7 @@ def generate_answer(query, context):
 
     # calling openAI
     response = client.chat.completions.create(
-        model="gpt-4o"
+        model="gpt-4o",
         message=[
             {"role", "system", "content", system_prompt},
             {"role": "user", "content": user_message}
